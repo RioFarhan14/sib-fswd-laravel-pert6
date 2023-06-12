798ecc38b3466f4cd6ba4768e30a975d6a7371ab
RioFarhan14 <riofarhan383@gmail.com>
2023-06-12 14:55:14 +0700

    [add] seeder

--- app/Http/Controllers/DasboardController.php
+++ app/Http/Controllers/DasboardController.php
@@ -8,9 +8,29 @@ use Illuminate\Support\Facades\Auth;
 
 class DasboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::with('category')->get();
+        $query = Product::query();
+
+        // Filter berdasarkan nama produk
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->input('name') . '%');
+        }
+
+        // Filter berdasarkan kategori
+        if ($request->has('category')) {
+            $query->whereHas('category', function ($q) use ($request) {
+                $q->where('name', $request->input('category'));
+            });
+        }
+
+        // Filter berdasarkan merek
+        if ($request->has('brand')) {
+            $query->where('brand', $request->input('brand'));
+        }
+
+        // Eksekusi query dan ambil hasilnya
+        $products = $query->with('category')->get();
         if (Auth::user()->role->name == 'user') {
             return view('Product.card', ['products' => $products]);
         } else {
